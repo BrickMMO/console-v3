cb51883a801aebc9d2315084e5d0e3dea8111be9
Arnulfo Sanchez <arnulfo457@gmail.com>
2024-07-23 11:29:25 -0400

    colours details api fixed

--- api/colours.details.php
+++ api/colours.details.php
@@ -1,47 +1,49 @@
 <?php
 
-$query = 'SELECT colours.id, colours.name, rgb, is_trans, rebrickable_id, externals.name AS external_name, source
+if(isset($_GET['key'])){
+    $query = 'SELECT colours.id, colours.name, rgb, is_trans, rebrickable_id, externals.name AS external_name, source
     FROM colours 
     INNER JOIN externals
     ON colours.id = externals.colour_id
-    WHERE colours.id = 65';    
+    WHERE colours.id = '.$_GET["key"];    
 
-$result = mysqli_query($connect, $query);
+    $result = mysqli_query($connect, $query);
 
-if ($result) {
+    if ($result) {
 
-    header("Content-type: JSON");
+        header("Content-type: JSON");
 
-    $colourObject = mysqli_fetch_assoc($result);
+        $colourObject = mysqli_fetch_assoc($result);
 
-    $colour = array(
-        'id' => $colourObject['id'],
-        'name' => $colourObject['name'],
-        'rgb' => $colourObject['rgb'],
-        'is_trans' => $colourObject['is_trans'],
-        'rebrickable_id' => $colourObject['rebrickable_id'],
-        'external_ids' => array(),
-    );
+        $colour = array(
+            'id' => $colourObject['id'],
+            'name' => $colourObject['name'],
+            'rgb' => $colourObject['rgb'],
+            'is_trans' => $colourObject['is_trans'],
+            'rebrickable_id' => $colourObject['rebrickable_id'],
+            'external_ids' => array(),
+        );
 
-    mysqli_data_seek($result, 0);
+        mysqli_data_seek($result, 0);
 
-    while ($externals = mysqli_fetch_assoc($result)) {
-        $colour['external_ids'][] = array(
-            'source' => $externals['source'],
-            'name' => $externals['external_name']
+        while ($externals = mysqli_fetch_assoc($result)) {
+            $colour['external_ids'][] = array(
+                'source' => $externals['source'],
+                'name' => $externals['external_name']
+            );
+        }
+
+        $data = array(
+            'message' => 'Colours retrieved successfully.',
+            'error' => false, 
+            'colours' => $colour,
+        );
+
+    } else {
+        $data = array(
+            'message' => 'Error retrieving colours detail.',
+            'error' => true,
+            'colours' => null,
         );
     }
-
-    $data = array(
-        'message' => 'Colours retrieved successfully.',
-        'error' => false, 
-        'colours' => $colour,
-    );
-
-} else {
-    $data = array(
-        'message' => 'Error retrieving colours detail.',
-        'error' => true,
-        'colours' => null,
-    );
 }
