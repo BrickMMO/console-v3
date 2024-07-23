6dfde00d491c98aa2c494be0c306135a7d8ff503
Arnulfo Sanchez <arnulfo457@gmail.com>
2024-07-22 23:42:34 -0400

    skeleton colours details api

--- api/colours.php
+++ api/colours.php
@@ -2,24 +2,35 @@
 
 $query = 'SELECT * 
     FROM colours 
-    ORDER BY name';    
+    ORDER BY name'; 
+
 $result = mysqli_query($connect, $query);
 
 $colorArray = array();
 
 if($result){
+
     header("Content-type: JSON");
+
     $i = 0;
+
     while($colour = mysqli_fetch_assoc($result)){
         $colorArray[$i]['id'] = $colour['id'];
         $colorArray[$i]['name'] = $colour['name'];
         $colorArray[$i]['rgb'] = $colour['rgb'];
         $i++;
     }
-}
 
-$data = array(
-    'message' => 'Colours retrieved successfully.',
-    'error' => false, 
-    'colours' => $colorArray,
-);
+    $data = array(
+        'message' => 'Colours retrieved successfully.',
+        'error' => false, 
+        'colours' => $colorArray,
+    );
+    
+} else {
+    $data = array(
+        'message' => 'Error retrieving colours detail.',
+        'error' => true,
+        'colours' => null,
+    );
+}
